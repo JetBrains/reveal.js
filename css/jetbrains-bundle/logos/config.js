0fbb5c7e4c158b9b3bb706eabaa2fa67c2698549
gorohoroh <gorohoroh@gmail.com>
2016-02-26 17:25:17 +0300

    Added current version of JetBrains bundle

---
+++ css/jetbrains-bundle/logos/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  logos: [
+    {'jetbrains': 'jetbrains/jetbrains.svg'},
+    {'jetbrains-square': 'jetbrains/jetbrains-simple.svg'},
+    {'appcode': 'appcode/appcode.svg'},
+    {'clion': 'clion/clion.svg'},
+    {'datagrip': 'datagrip/datagrip.svg'},
+    {'dotcover': 'dotcover/dotcover.svg'},
+    {'dotmemory': 'dotmemory/dotmemory.svg'},
+    {'dotpeek': 'dotpeek/dotpeek.svg'},
+    {'dottrace': 'dottrace/dottrace.svg'},
+    {'hub': 'hub/hub.svg'},
+    {'intellij-idea': 'intellij-idea/intellij-idea.svg'},
+    {'kotlin': 'kotlin/kotlin-retro.svg'},
+    {'mps': 'mps/mps.svg'},
+    {'phpstorm': 'phpstorm/phpstorm.svg'},
+    {'pycharm': 'pycharm/pycharm.svg'},
+    {'pycharm-edu': 'pycharm-edu/pycharm-edu.svg'},
+    {'resharper': 'resharper/resharper.svg'},
+    {'resharper-cpp': 'resharper-cpp/resharper-cpp.svg'},
+    {'rubymine': 'rubymine/rubymine.svg'},
+    {'teamcity': 'teamcity/teamcity.svg'},
+    {'upsource': 'upsource/upsource.svg'},
+    {'webstorm': 'webstorm/webstorm.svg'},
+    {'youtrack': 'youtrack/youtrack.svg'}
+  ],
+
+  sizes: {
+    'size-1': 40,
+    'size-2': 60,
+    'size-3': 75,
+    'size-4': 100,
+    'size-5': 150
+  }
+};
