c6ac96915277f5d2ec49de58c76c7d360ab1e644
gorohoroh <gorohoroh@gmail.com>
2016-05-03 01:20:32 +0300

    Updated JetBrains bundle

---
+++ css/jetbrains-bundle/logos/text/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  logos: {
+    'appcode-text': 'appcode/appcode-text.svg',
+    'clion-text': 'clion/clion-text.svg',
+    'datagrip-text': 'datagrip/datagrip-text.svg',
+    'dotcover-text': 'dotcover/dotcover-text.svg',
+    'dotmemory-text': 'dotmemory/dotmemory-text.svg',
+    'dotpeek-text': 'dotpeek/dotpeek-text.svg',
+    'dottrace-text': 'dottrace/dottrace-text.svg',
+    'hub-text': 'hub/hub-text.svg',
+    'intellij-idea-text': 'intellij-idea/intellij-idea-text.svg',
+    'mps-text': 'mps/mps-text.svg',
+    'phpstorm-text': 'phpstorm/phpstorm-text.svg',
+    'pycharm-text': 'pycharm/pycharm-text.svg',
+    'pycharm-edu-text': 'pycharm-edu/pycharm-edu-text.svg',
+    'rider-text': 'rider/rider-text.svg',
+    'resharper-text': 'resharper/resharper-text.svg',
+    'resharper-cpp-text': 'resharper-cpp/resharper-cpp-text.svg',
+    'resharper-ultimate-text': 'resharper-ultimate/resharper-ultimate-text.svg',
+    'rubymine-text': 'rubymine/rubymine-text.svg',
+    'teamcity-text': 'teamcity/teamcity-text.svg',
+    'upsource-text': 'upsource/upsource-text.svg',
+    'webstorm-text': 'webstorm/webstorm-text.svg',
+    'youtrack-text': 'youtrack/youtrack-text.svg'
+  },
+
+  sizes: {
+    'size-1': {height: 60},
+    'size-2': {height: 90},
+    'size-3': {height: 120}
+  }
+};
