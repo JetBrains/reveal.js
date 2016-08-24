2c93f0e685f8f5a327e307d7fb6919134f70a836
kisenka <qtuzov@gmail.com>
2016-08-24 18:59:32 +0300

    JS-4904 Booth demos: an easy way to create playlists

--- js/jetbrains-slides.js
+++ js/jetbrains-slides.js
@@ -1,67 +1,92 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var styleMappings = [
-        ['resharper-slide', 'resharper-background'],
-        ['dottrace-slide', 'dottrace-background'],
-        ['dotcover-slide', 'dotcover-background'],
-        ['dotmemory-slide', 'dotmemory-background'],
-        ['dotpeek-slide', 'dotpeek-background'],
-        ['resharper-cpp-slide', 'resharper-cpp-background'],
-        ['rider-slide', 'rider-background'],
-        ['webstorm-slide', 'webstorm-background'],
-        ['appcode-slide', 'appcode-background'],
-        ['clion-slide', 'clion-background'],
-        ['intellij-slide', 'intellij-background'],
-        ['phpstorm-slide', 'phpstorm-background'],
-        ['pycharm-slide', 'pycharm-background'],
-        ['rubymine-slide', 'rubymine-background'],
-        ['teamcity-slide', 'teamcity-background'],
-        ['upsource-slide', 'upsource-background'],
-        ['youtrack-slide', 'youtrack-background'],
-        ['kotlin-slide', 'kotlin-background'],
-        ['datagrip-slide', 'datagrip-background'],
-        ['hub-slide', 'hub-background'],
-        ['mps-slide', 'mps-background'],
-    ];
+  var styleMappings = [
+    ['resharper-slide', 'resharper-background'],
+    ['dottrace-slide', 'dottrace-background'],
+    ['dotcover-slide', 'dotcover-background'],
+    ['dotmemory-slide', 'dotmemory-background'],
+    ['dotpeek-slide', 'dotpeek-background'],
+    ['resharper-cpp-slide', 'resharper-cpp-background'],
+    ['rider-slide', 'rider-background'],
+    ['webstorm-slide', 'webstorm-background'],
+    ['appcode-slide', 'appcode-background'],
+    ['clion-slide', 'clion-background'],
+    ['intellij-slide', 'intellij-background'],
+    ['phpstorm-slide', 'phpstorm-background'],
+    ['pycharm-slide', 'pycharm-background'],
+    ['rubymine-slide', 'rubymine-background'],
+    ['teamcity-slide', 'teamcity-background'],
+    ['upsource-slide', 'upsource-background'],
+    ['youtrack-slide', 'youtrack-background'],
+    ['kotlin-slide', 'kotlin-background'],
+    ['datagrip-slide', 'datagrip-background'],
+    ['hub-slide', 'hub-background'],
+    ['mps-slide', 'mps-background'],
+  ];
 
+  selectBackground(styleMappings);
+
+  // select the target node
+  var target = document.querySelector('section');
+
+  // create an observer instance
+  var observer = new MutationObserver(function (mutations) {
     selectBackground(styleMappings);
+  });
 
-    // select the target node
-    var target = document.querySelector('section');
+  // configuration of the observer:
+  var config = {attributes: true, childList: false, characterData: false};
 
-    // create an observer instance
-    var observer = new MutationObserver(function(mutations) {
-        selectBackground(styleMappings);
-    });
-
-    // configuration of the observer:
-    var config = { attributes: true, childList: false, characterData: false };
-
-    // pass in the target node, as well as the observer options
-    observer.observe(target, config);
-
+  // pass in the target node, as well as the observer options
+  observer.observe(target, config);
 });
 
 function selectBackground(styleMappings) {
-    var currentSection = $('section.present');
+  var currentSection = $('section.present');
 
-    var matchingStyles = [];
+  var matchingStyles = [];
 
-    for (var i = 0; i < styleMappings.length; i++) {
+  for (var i = 0; i < styleMappings.length; i++) {
 
-        var oneOfValidSectionStyles = styleMappings[i][0];
+    var oneOfValidSectionStyles = styleMappings[i][0];
 
-        if ($(currentSection).hasClass(oneOfValidSectionStyles)) {
-            matchingStyles.push(oneOfValidSectionStyles);
+    if ($(currentSection).hasClass(oneOfValidSectionStyles)) {
+      matchingStyles.push(oneOfValidSectionStyles);
 
-            $('body').removeClass().addClass(styleMappings[i][1]);
+      $('body').removeClass().addClass(styleMappings[i][1]);
 
-            break;
-        }
+      break;
     }
+  }
 
-    if (matchingStyles.length === 0) {
-        $('body').removeClass().addClass('default-background');
-    }
+  if (matchingStyles.length === 0) {
+    $('body').removeClass().addClass('default-background');
+  }
 }
 
+function handleActiveProducts() {
+  if (!activeProducts) {
+    return;
+  }
+
+  var slidesToRemove = [];
+  var products = [];
+
+  $('.slides section').each(function () {
+    var $slide = $(this);
+    var slideProduct = $slide.attr('data-product');
+    products.push(slideProduct);
+
+    if (activeProducts.indexOf(slideProduct) == -1) {
+      slidesToRemove.push($slide);
+    }
+  });
+
+  products = $.unique(products);
+
+  $(slidesToRemove).each(function () {
+    $(this).remove();
+  });
+
+  console.log('Available products: %s', products.join(', '));
+}
---
+++ js/slides-playlist.js
@@ -0,0 +1,6 @@
+var conferencesByTypes = {
+  wwdc: ['appcode'],
+  dotnet: ['resharper', 'resharper-cpp', 'dottrace', 'dotcover', 'dotmemory', 'dotpeek']
+};
+
+var activeProducts = conferencesByTypes.wwdc;
