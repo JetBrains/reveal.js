6fd33593bd97e73cea12677e8a02e5a659dfdd85
Artem Sarkisov <artem.sarkisov@jetbrains.com>
2017-01-19 15:49:43 +0300

    changed currentPlaylist back to .dotnet

--- js/playlist.js
+++ js/playlist.js
@@ -53,4 +53,4 @@ var conferenceType = {
 // to switch on a playlist for a specific conference type,ª
 // assign it to the currentPlaylist variable, i.e.:
 // var currentPlaylist = conferenceType.dotnet;
-var currentPlaylist = conferenceType.web;
+var currentPlaylist = conferenceType.dotnet;
