de8eccd21be9de00fc77126493a3f7f4a98c7bf7
cheptsov <andrey.cheptsov@jetbrains.com>
2018-09-24 17:06:53 +0200

    Switched the default playlist to Java. This is dirty a workaround. The way the playlist is managed has to be improved. // cc shkate, cheptsov

--- js/playlist.js
+++ js/playlist.js
@@ -93,4 +93,4 @@ var conferenceType = {
 // to switch on a playlist for a specific conference type,ª
 // assign it to the currentPlaylist variable, i.e.:
 // var currentPlaylist = conferenceType.dotnet;
-var currentPlaylist = conferenceType.velocity;
+var currentPlaylist = conferenceType.java;
